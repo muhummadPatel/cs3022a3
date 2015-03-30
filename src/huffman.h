220bdceeae8a616da08523b5aa63df78d5630200
Muhummad Patel <muhummad.patel@gmail.com>
2015-03-30 22:09:03 +0200

    Add accessor method definitions for HuffmanNode class and a genCodeTbl method definition for the HuffmanTree class.

--- src/huffman.h
+++ src/huffman.h
@@ -2,6 +2,7 @@
 #define HUFFMAN_H
 
 #include <memory>
+#include <unordered_map>
 
 namespace ptlmuh006{
 
@@ -13,25 +14,31 @@ namespace ptlmuh006{
             std::shared_ptr<HuffmanNode> rightChild;
 
         public:
-            HuffmanNode(char dat = '\0', int freq = 0): data(dat), frequency(freq){};
+            HuffmanNode(char dat = '\0', int freq = 0): data(dat), frequency(freq), leftChild(nullptr), rightChild(nullptr){};
             //TODO: oher RAII methods here
 
             char getData();
+            int getFrequency();
+            void setData(char);
+            void setFrequency(int);
             void setLeftChild(std::shared_ptr<HuffmanNode>);
             void setRightChild(std::shared_ptr<HuffmanNode>);
             std::shared_ptr<HuffmanNode> getLeftChild();
             std::shared_ptr<HuffmanNode> getRightChild();
+            //static bool compare(HuffmanNode, HuffmanNode);
     };
 
     class HuffmanTree{
         private:
             std::shared_ptr<HuffmanNode> root;
+//            std::unordered_map<char, std::string> codeTbl;
 
         public:
             HuffmanTree(): root(nullptr){};
             //TODO: other RAII methods here
 
             void build(std::string data);
+            void genCodeTbl(std::shared_ptr<HuffmanNode>, std::string);
     };
 
 }
