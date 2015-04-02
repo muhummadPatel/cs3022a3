cb380ef9bb705bcc7f18e9fced06dd9202c3d201
Muhummad Patel <muhummad.patel@gmail.com>
2015-04-02 19:57:22 +0200

    Add command line parsing.

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 
-#include "reader.h"
 #include "huffman.h"
 
-int main(int argc, const char* argv[]) {
+int main(int argc, char* argv[]) {
     using namespace ptlmuh006;
 
-    //std::string fil = readFile("test.txt");
-    HuffmanTree ht;
-    //ht.build(fil);
-    ht.compress("test.txt", "testout");
-    ht.extract("testout");
+    if(argc != 4){
+        std::cout << "Incorrect usage." << std::endl;
+        std::cout << "Please use huffencode as follows:" << std::endl;
+        std::cout << "huffencode <option> <inputFilename> <outputFilename>" << std::endl;
+        std::cout << "\t___options:___" << std::endl;
+        std::cout << "\t -c compress data from inputFilename to outputFilename" << std::endl;
+        std::cout << "\t -x extract compressed data from inputFilename to outputFilename" << std::endl;
 
+    }else{
+        HuffmanTree* huff = new HuffmanTree;
+
+        std::string option = argv[1];
+        if(option == "-c"){
+            huff->compress(argv[2], argv[3]);
+            std::cout << "Compressed " << argv[2] << " into " << argv[3] << "." << std::endl;
+
+        }else if(option == "-x"){
+            huff->extract(argv[2], argv[3]);
+            std::cout << "Extracted from " << argv[2] << " into " << argv[3] << "." << std::endl;
+        }
+
+        delete huff;
+    }
     return 0;
 }
